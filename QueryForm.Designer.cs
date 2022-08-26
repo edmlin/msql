c24240138787814ae698171aeef9dd842e9aee7c
Lin, Yan (Y.M.) <YLIN68@ford.com>
2022-08-26 14:52:18 -0400

    Small improvement

--- QueryForm.Designer.cs
+++ QueryForm.Designer.cs
@@ -47,182 +47,184 @@ namespace msql
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.tbSql = new System.Windows.Forms.TextBox();
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.numPage = new System.Windows.Forms.NumericUpDown();
-			this.label2 = new System.Windows.Forms.Label();
-			this.numRecords = new System.Windows.Forms.NumericUpDown();
-			this.label1 = new System.Windows.Forms.Label();
-			this.btClose = new System.Windows.Forms.Button();
-			this.btRun = new System.Windows.Forms.Button();
-			this.groupBox1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.Panel1.SuspendLayout();
-			this.splitContainer1.Panel2.SuspendLayout();
-			this.splitContainer1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-			this.panel1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.numPage)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.numRecords)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Controls.Add(this.tbSql);
-			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox1.Location = new System.Drawing.Point(0, 0);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(524, 68);
-			this.groupBox1.TabIndex = 0;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "SQL";
-			// 
-			// tbSql
-			// 
-			this.tbSql.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tbSql.Location = new System.Drawing.Point(3, 16);
-			this.tbSql.Multiline = true;
-			this.tbSql.Name = "tbSql";
-			this.tbSql.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.tbSql.Size = new System.Drawing.Size(518, 49);
-			this.tbSql.TabIndex = 0;
-			// 
-			// splitContainer1
-			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
-			this.splitContainer1.Size = new System.Drawing.Size(524, 298);
-			this.splitContainer1.SplitterDistance = 68;
-			this.splitContainer1.TabIndex = 1;
-			this.splitContainer1.TabStop = false;
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.AllowUserToAddRows = false;
-			this.dataGridView1.AllowUserToDeleteRows = false;
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.ReadOnly = true;
-			this.dataGridView1.Size = new System.Drawing.Size(524, 226);
-			this.dataGridView1.TabIndex = 0;
-			this.dataGridView1.TabStop = false;
-			// 
-			// panel1
-			// 
-			this.panel1.Controls.Add(this.numPage);
-			this.panel1.Controls.Add(this.label2);
-			this.panel1.Controls.Add(this.numRecords);
-			this.panel1.Controls.Add(this.label1);
-			this.panel1.Controls.Add(this.btClose);
-			this.panel1.Controls.Add(this.btRun);
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel1.Location = new System.Drawing.Point(0, 298);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(524, 52);
-			this.panel1.TabIndex = 2;
-			// 
-			// numPage
-			// 
-			this.numPage.Location = new System.Drawing.Point(193, 15);
-			this.numPage.Minimum = new decimal(new int[] {
-			1,
-			0,
-			0,
-			0});
-			this.numPage.Name = "numPage";
-			this.numPage.Size = new System.Drawing.Size(47, 20);
-			this.numPage.TabIndex = 5;
-			this.numPage.Value = new decimal(new int[] {
-			1,
-			0,
-			0,
-			0});
-			this.numPage.ValueChanged += new System.EventHandler(this.NumPageValueChanged);
-			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(156, 17);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(74, 22);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "Page:";
-			// 
-			// numRecords
-			// 
-			this.numRecords.Location = new System.Drawing.Point(83, 15);
-			this.numRecords.Name = "numRecords";
-			this.numRecords.Size = new System.Drawing.Size(53, 20);
-			this.numRecords.TabIndex = 2;
-			this.numRecords.Value = new decimal(new int[] {
-			50,
-			0,
-			0,
-			0});
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(3, 17);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(83, 20);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "Records/Page:";
-			// 
-			// btClose
-			// 
-			this.btClose.Location = new System.Drawing.Point(418, 12);
-			this.btClose.Name = "btClose";
-			this.btClose.Size = new System.Drawing.Size(75, 23);
-			this.btClose.TabIndex = 1;
-			this.btClose.Text = "Close";
-			this.btClose.UseVisualStyleBackColor = true;
-			this.btClose.Click += new System.EventHandler(this.BtCloseClick);
-			// 
-			// btRun
-			// 
-			this.btRun.Location = new System.Drawing.Point(327, 12);
-			this.btRun.Name = "btRun";
-			this.btRun.Size = new System.Drawing.Size(75, 23);
-			this.btRun.TabIndex = 0;
-			this.btRun.Text = "Run";
-			this.btRun.UseVisualStyleBackColor = true;
-			this.btRun.Click += new System.EventHandler(this.BtRunClick);
-			// 
-			// QueryForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(524, 350);
-			this.Controls.Add(this.splitContainer1);
-			this.Controls.Add(this.panel1);
-			this.Name = "QueryForm";
-			this.Text = "QueryForm";
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.QueryFormFormClosing);
-			this.groupBox1.ResumeLayout(false);
-			this.groupBox1.PerformLayout();
-			this.splitContainer1.Panel1.ResumeLayout(false);
-			this.splitContainer1.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-			this.panel1.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.numPage)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.numRecords)).EndInit();
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QueryForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbSql = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numPage = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numRecords = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btClose = new System.Windows.Forms.Button();
+            this.btRun = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbSql);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 68);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SQL";
+            // 
+            // tbSql
+            // 
+            this.tbSql.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbSql.Location = new System.Drawing.Point(3, 16);
+            this.tbSql.Multiline = true;
+            this.tbSql.Name = "tbSql";
+            this.tbSql.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbSql.Size = new System.Drawing.Size(518, 49);
+            this.tbSql.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(524, 298);
+            this.splitContainer1.SplitterDistance = 68;
+            this.splitContainer1.TabIndex = 1;
+            this.splitContainer1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(524, 226);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numPage);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numRecords);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btClose);
+            this.panel1.Controls.Add(this.btRun);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 298);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(524, 52);
+            this.panel1.TabIndex = 2;
+            // 
+            // numPage
+            // 
+            this.numPage.Location = new System.Drawing.Point(193, 15);
+            this.numPage.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPage.Name = "numPage";
+            this.numPage.Size = new System.Drawing.Size(47, 20);
+            this.numPage.TabIndex = 5;
+            this.numPage.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPage.ValueChanged += new System.EventHandler(this.NumPageValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(156, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Page:";
+            // 
+            // numRecords
+            // 
+            this.numRecords.Location = new System.Drawing.Point(83, 15);
+            this.numRecords.Name = "numRecords";
+            this.numRecords.Size = new System.Drawing.Size(53, 20);
+            this.numRecords.TabIndex = 2;
+            this.numRecords.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Records/Page:";
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(418, 12);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 1;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.BtCloseClick);
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(327, 12);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(75, 23);
+            this.btRun.TabIndex = 0;
+            this.btRun.Text = "Run";
+            this.btRun.UseVisualStyleBackColor = true;
+            this.btRun.Click += new System.EventHandler(this.BtRunClick);
+            // 
+            // QueryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 350);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "QueryForm";
+            this.Text = "QueryForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.QueryFormFormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numPage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRecords)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 	}
